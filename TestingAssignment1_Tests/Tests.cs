9b4f3b23b27e260697d4f2480c79f77e48ad0bd3
Madan <mgurrala9086@conestogac.on.ca>
2019-05-30 01:41:41 -0400

    Completed Unit tests

--- TestingAssignment1_Tests/Tests.cs
+++ TestingAssignment1_Tests/Tests.cs
@@ -27,5 +27,76 @@ namespace TestingAssignment1_Tests
 
         }
 
+        [Test]
+
+        public void Set_Length_Testing()
+        {
+            //Arrange
+            int l = 5;
+            int w = 6;
+            Rectangle rect = new Rectangle(l, w);
+            //Act
+            int length = rect.SetLength(5);
+
+            //Assert
+            Assert.AreEqual(length, l);
+        }
+
+        [Test]
+        public void Get_Width_Testing()
+        {
+            //Arrange
+            int l = 15;
+            int w = 27;
+            Rectangle rect = new Rectangle(l, w);
+            //Act
+            int width = rect.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, w);
+        }
+
+        [Test]
+        public void Set_Width_Testing()
+        {
+            //Arrange
+            //int l = 34;
+            //int w = 56;
+            Rectangle rect = new Rectangle(34, 56);
+            //Act
+            int width = rect.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, 56);
+        }
+
+        [Test]
+        public void Get_Perimeter_Testing()
+        {
+            //Arrange
+            int l = 34;
+            int w = 56;
+            Rectangle rect = new Rectangle(l, w);
+            //Act
+            int permiter = rect.GetPerimeter();
+
+            //Assert
+            Assert.AreEqual(permiter, 180);
+        }
+
+        [Test]
+        public void Get_Area_Testing()
+        {
+            //Arrange
+            int l = 34;
+            int w = 87;
+            Rectangle rect = new Rectangle(l, w);
+            //Act
+            int area = rect.GetArea();
+
+            //Assert
+            Assert.AreEqual(area, 2958);
+        }
+
     }
 }
